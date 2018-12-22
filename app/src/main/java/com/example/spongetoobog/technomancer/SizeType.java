d6b56b584dbaae7c2bb9d8a06c8ad21a06fbcebf
nickgoldberg42 <nick.goldberg42@gmail.com>
2018-12-22 12:05:36 -0700

    Added database

---
+++ app/src/main/java/com/example/spongetoobog/technomancer/SizeType.java
@@ -0,0 +1,13 @@
+package com.example.spongetoobog.technomancer;
+
+public enum SizeType {
+    COLOSSAL,
+    GARGANTUAN,
+    HUGE,
+    LARGE,
+    MEDIUM,
+    SMALL,
+    TINY,
+    DIMINUTIVE,
+    FINE
+}
